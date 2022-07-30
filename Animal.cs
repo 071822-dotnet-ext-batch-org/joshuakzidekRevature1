76e31f438a6e40abc3d12e700002e17326b5a076
joshuazidekrevature1 <109550805+joshuazidekrevature1@users.noreply.github.com>
2022-07-30 17:24:08 -0400

    Add files via upload
    Code works if you make a few changes , and I changed it around so it would not work so I could try and fix it tomorrow for practice.

---
+++ Animal.cs
@@ -0,0 +1,17 @@
+using system;
+
+namespace polyinherit
+{
+    public class Animal // Base class (parent)
+}
+        public virtual void animalSound()
+        {
+            Console.WriteLine("how many limbs does the animal have?");
+        }
+
+        public virtual void animalLimbs()
+        {
+            Console.WriteLine("How many limbs does the animal have?")
+        }
+        {
+        }//Eon
